dce323520ece249ae07236fb55d643ebc3179564
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-06-01 17:36:35 +0500

    Updated version to 3.0 for further development

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Server Info
 Description: This plugin will show you useful information about the hosting server you are using e.g. PHP version, MySQL version, Server OS, Server Protocol, Server IP and other useful information. You can use the information displayed by this plugin to update any settings which is crucial for your website performance and other aspects.
-Version: 2.0
+Version: 3.0
 Author: Usman Ali Qureshi
 Author URI: https://profiles.wordpress.org/usmanaliqureshi
 License: GPLv2 or later
