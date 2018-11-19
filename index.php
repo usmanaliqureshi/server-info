80b38fe8a1406a2bc13e38cc17ac801acf7d964e
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2018-11-19 16:12:48 +0500

    Update: Plugin Information

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-/*
-Plugin Name: Server Info
-Description: This plugin will show you useful information about the hosting server you are using e.g. PHP version, MySQL version, Server OS, Server Protocol, Server IP and other useful information. You can use the information displayed by this plugin to update any settings which is crucial for your website performance and other aspects.
-Version: 2.5
-Author: Usman Ali Qureshi
-Author URI: https://profiles.wordpress.org/usmanaliqureshi
-License: GPLv2 or later
-*/
-
+/**
+  * Plugin Name: Server Info
+  * Description: This plugin will show you useful information about the hosting server you are using e.g. PHP version, MySQL version, Server OS, Server Protocol, Server IP and other useful information. You can use the information displayed by this plugin to update any settings which is crucial for your website performance and other aspects.
+  * Plugin URI: https://github.com/usmanaliqureshi/server-info
+  * Version: 2.5
+  * Author: Usman Ali Qureshi
+  * Author URI: https://profiles.wordpress.org/usmanaliqureshi
+  * License: GPLv2 or later
+  * Text Domain: si
+  * Domain Path: /languages/
+  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
