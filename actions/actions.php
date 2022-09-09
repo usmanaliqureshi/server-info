3113df75f1eff264953b89b698c9b1500aaad7f5
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2022-09-09 16:48:41 +0500

    Removed unnecessary spacing

--- actions/actions.php
+++ actions/actions.php
@@ -2,8 +2,7 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+// Actions
 add_action( 'admin_menu', array( 'server_info', 'servinfo_admin_actions' ) );
-
 add_action( 'admin_head', array( 'server_info', 'infohouse_css_styles' ) );
-
 add_action( 'wp_dashboard_setup', array( 'server_info', 'server_info_add_dashboard_widgets' ) );
